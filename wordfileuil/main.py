94240f575430ad8f45ada28b967a9ccfd07bded8
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2026-03-30 07:24:17 +0000

    made changes

--- wordfileuil/main.py
+++ wordfileuil/main.py
@@ -1,10 +1,28 @@
 from wordcount import wordcount
 from nospace import nospace
+from toupperandlower import changetolower, changetoupper
 
 def main():
-    text = input()
-    print(wordcount(text))
-    print(nospace(text))
+    while True:
+        print("CHOOSE YOUR OPTION: \n1. Word Count\n2. No Space \n3. To Upper \n4. To Lower \n5. Exit")
+        opt = int(input("\nEnter your option : "))
+        if opt==5:
+            print("BYE BYE")
+            break
+        text = input("Enter the text : ")
+        match(opt):
+            case 1:
+                print(wordcount(text))
+                break
+            case 2:
+                print(nospace(text))
+                break
+            case 3:
+                print(changetoupper(text))
+                break
+            case 4:
+                print(changetolower(text))
+                break
 
-def __name__=="__main__":
+if __name__=="__main__":
     main()
---
+++ wordfileuil/toupperandlower.py
@@ -0,0 +1,5 @@
+def changetoupper(text):
+    return text.upper()
+
+def changetolower(text):
+    return text.lower()
