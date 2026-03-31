1bd2acd81ab01f7e543fb4e9de0da8a78e679757
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2026-03-31 18:00:37 +0000

    updated the main file

--- wordfileuil/main.py
+++ wordfileuil/main.py
@@ -1,15 +1,18 @@
 from wordcount import wordcount
 from nospace import nospace
 from toupperandlower import changetolower, changetoupper
+from wordlistgenerator import generate
+from wordlistcleaner import clean_wordlist
 
 def main():
     while True:
-        print("CHOOSE YOUR OPTION: \n1. Word Count\n2. No Space \n3. To Upper \n4. To Lower \n5. Exit")
+        print("CHOOSE YOUR OPTION: \n1. Word Count\n2. No Space \n3. To Upper \n4. To Lower \n5. Wordlist generate \n6. Wordlist clean \n7. Exit")
         opt = int(input("\nEnter your option : "))
-        if opt==5:
+        if opt==7:
             print("BYE BYE")
             break
-        text = input("Enter the text : ")
+        if opt!=6:
+            text = input("Enter the text : ")
         match(opt):
             case 1:
                 print(wordcount(text))
@@ -22,6 +25,14 @@ def main():
                 break
             case 4:
                 print(changetolower(text))
+                break
+            case 5:
+                print(generate(text))
+                break
+            case 6:
+                print("Give absolute address of Input file : ")
+                inter = input()
+                print(clean_wordlist(inter,output))
                 break
 
 if __name__=="__main__":
