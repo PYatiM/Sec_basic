e2e6d56538bfcf94eba664928f07e0dde524b458
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2026-04-06 16:57:02 +0000

    hash generator added

---
+++ wordfileuil/hashgenerator.py
@@ -0,0 +1,18 @@
+import hashlib
+
+def generate_hashes(input_file, output_file, algo='sha256'):
+    with open(input_file, 'r') as f, open(output_file, 'w') as out:
+        for line in f:
+            word = line.strip().encode()
+
+            if algo == 'md5':
+                h = hashlib.md5(word).hexdigest()
+            elif algo == 'sha1':
+                h = hashlib.sha1(word).hexdigest()
+            else:
+                h = hashlib.sha256(word).hexdigest()
+
+            out.write(f"{word.decode()}:{h}\n")
+
+if __name__ == "__main__":
+    generate_hashes("input.txt", "hashes.txt", "sha256")
