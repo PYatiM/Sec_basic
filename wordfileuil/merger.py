6c2017ad871b71971d98588a73c925ef95a3f8e1
P YATHIN MACHAIAH <p.yathinmachaiah18@gmail.com>
2026-04-06 16:54:16 +0000

    merger file added

---
+++ wordfileuil/merger.py
@@ -0,0 +1,14 @@
+import sys
+
+def merge_files(output_file, *input_files):
+    words = set()
+    for file in input_files:
+        with open(file, 'r', encoding='utf-8', errors='ignore') as f:
+            words.update(line.strip() for line in f if line.strip())
+
+    with open(output_file, 'w') as f:
+        for word in sorted(words):
+            f.write(word + '\n')
+
+if __name__ == "__main__":
+    merge_files("merged.txt", "file1.txt", "file2.txt", "file3.txt")
